27e8c57d54dc546718596b2db1840ce4b6deb06f
Mariana <pasqualinimari@gmail.com>
2022-07-11 20:02:50 -0300

    feat: predict 2020 data

--- brasileirao_2020.R
+++ brasileirao_2020.R
@@ -10,47 +10,47 @@ source("src/plot_functions.R")
 time_ids <- fread("data/times_ids.csv") |> distinct(id, .keep_all = TRUE)
 time_ids <- time_ids[ , id, nome.cartola]
 
-brasileirao_2019 <- fread("data/2019_partidas.csv")
+brasileirao_2020 <- fread("data/2020_partidas.csv") |> distinct()
 
-brasileirao_2019 <- brasileirao_2019 |> 
+brasileirao_2020 <- brasileirao_2020 |> 
   left_join(time_ids, by = c("home_team" = "id")) |> 
   left_join(time_ids, by = c("away_team" = "id")) |> 
   rename(home_team_name = "nome.cartola.x",
          away_team_name = "nome.cartola.y")
 
 # Preparing original data ----
-brasileirao_2019 <- reindex_teams(brasileirao_2019)
+brasileirao_2020 <- reindex_teams(brasileirao_2020)
 
-brasileirao_2019 <- brasileirao_2019 |> 
+brasileirao_2020 <- brasileirao_2020 |> 
   rename(y1 = "home_score", 
          y2 = "away_score",
          h = "home_team_index", 
          a = "away_team_index")
 
-score_brasileirao_2019 <- 
-  get_team_points_per_game(data = brasileirao_2019, 
+score_brasileirao_2020 <- 
+  get_team_points_per_game(data = brasileirao_2020, 
                            y1 = quo(y1), 
                            y2 = quo(y2))
 
 # cols <- c("home_team", "away_team", "home_score", "away_score", 
 #          "home_team_index", "away_team_index")
 
-# brasileirao_2019[ , ..cols] |> head() |> saveRDS(file = "dataframe_head.rds")
+# brasileirao_2020[ , ..cols] |> head() |> saveRDS(file = "dataframe_head.rds")
 
 # Model 1 ----
 
-brasileirao_2019_list <- list(G = nrow(brasileirao_2019), 
+brasileirao_2020_list <- list(G = nrow(brasileirao_2020), 
                               T = 20,
-                              h = brasileirao_2019$h,
-                              a = brasileirao_2019$a,
-                              y1 = brasileirao_2019$y1,
-                              y2 = brasileirao_2019$y2)
+                              h = brasileirao_2020$h,
+                              a = brasileirao_2020$a,
+                              y1 = brasileirao_2020$y1,
+                              y2 = brasileirao_2020$y2)
 
 m1 <- stan_model(file = "models/modelo1-poisson-hierarchical.stan", 
                  model_name = "model1-hierarchical")
 
 m1_fit <- sampling(object = m1, 
-                   data = brasileirao_2019_list, 
+                   data = brasileirao_2020_list, 
                    chains = 1, 
                    iter = 5000)
 
@@ -58,15 +58,15 @@ y_predict_m1 <- extract(m1_fit, pars = c("y1_tilde", "y2_tilde"))
 y_predict_m1_df <- data.frame(y1_pred_m1 = y_predict_m1$y1_tilde[1,], 
                               y2_pred_m1 = y_predict_m1$y2_tilde[1,])
 
-brasileirao_2019_with_pred <- cbind(brasileirao_2019, y_predict_m1_df)
+brasileirao_2020_with_pred <- cbind(brasileirao_2020, y_predict_m1_df)
 
-score_brasileirao_2019_m1_pred <- 
-  get_team_points_per_game(data = brasileirao_2019_with_pred, 
+score_brasileirao_2020_m1_pred <- 
+  get_team_points_per_game(data = brasileirao_2020_with_pred, 
                            y1 = quo(y1_pred_m1), 
                            y2 = quo(y2_pred_m1))
 
-points_per_game_obs_m1 <- merge(score_brasileirao_2019, 
-                                score_brasileirao_2019_m1_pred, 
+points_per_game_obs_m1 <- merge(score_brasileirao_2020, 
+                                score_brasileirao_2020_m1_pred, 
                                 by = c("game_id", "team_id", "team_name"), 
                                 suffixes = c("_obs", "_est_m1"))
 
@@ -79,24 +79,14 @@ pontuacao <- points_per_game_obs_m1 |>
   summarize(score_obs = sum(points_scored_obs), 
             score_est_m1 = sum(points_scored_est_m1))
 
-saveRDS(pontuacao, "artifacts/pontuacao_total_m1.rds")
+#saveRDS(pontuacao, "artifacts/pontuacao_total_m1.rds")
 
 ## MSE ----
 
-mse_m1 <- data.frame(mse_y1 = mse(brasileirao_2019_with_pred$y1, 
-                                  brasileirao_2019_with_pred$y1_pred_m1),
-                     mse_y2 = mse(brasileirao_2019_with_pred$y2, 
-                                  brasileirao_2019_with_pred$y2_pred_m1))
-
-#points_championship <- merge(score_brasileirao_2019, 
-#                             score_brasileirao_2019_pred, 
-#                             by = c("game_id", "team_id"), 
-#                             suffixes = c("_obs", "_est")) |> 
-#  group_by(team_id) |> 
-#  summarise(total_obs = sum(points_scored_obs),
-#            total_est = sum(points_scored_est), 
-#            mse = (total_obs - total_est)) |> 
-#  summarise(mean(total_obs - total_est)^2)
+mse_m1 <- data.frame(mse_y1 = mse(brasileirao_2020_with_pred$y1, 
+                                  brasileirao_2020_with_pred$y1_pred_m1),
+                     mse_y2 = mse(brasileirao_2020_with_pred$y2, 
+                                  brasileirao_2020_with_pred$y2_pred_m1))
 
 ## LOO ----
 
@@ -110,12 +100,12 @@ loo_y2_m1 <-loo(log_lik_y2_m1, r_eff = ref_y2_m1)
 
 # Model 2 ----
 
-brasileirao_2019_list_m2 <- list(G = nrow(brasileirao_2019), 
+brasileirao_2020_list_m2 <- list(G = nrow(brasileirao_2020), 
                                  T = 20,
-                                 h = brasileirao_2019$h,
-                                 a = brasileirao_2019$a,
-                                 y1 = brasileirao_2019$y1,
-                                 y2 = brasileirao_2019$y2,
+                                 h = brasileirao_2020$h,
+                                 a = brasileirao_2020$a,
+                                 y1 = brasileirao_2020$y1,
+                                 y2 = brasileirao_2020$y2,
                                  gamma1 = 0,
                                  gamma2 = 0)
 
@@ -123,7 +113,7 @@ m2 <- stan_model(file = "models/modelo2-poisson-bivariate.stan",
                  model_name = "model2-bivariate-gamma00")
 
 m2_fit <- sampling(object = m2, 
-                   data = brasileirao_2019_list_m2, 
+                   data = brasileirao_2020_list_m2, 
                    chains = 1, 
                    iter = 5000)
 
@@ -131,17 +121,17 @@ y_predict_m2 <- extract(m2_fit, pars = c("y1_tilde", "y2_tilde"))
 y_predict_m2_df <- data.frame(y1_pred_m2 = y_predict_m2$y1_tilde[1,], 
                               y2_pred_m2 = y_predict_m2$y2_tilde[1,])
 
-brasileirao_2019_with_pred <- cbind(brasileirao_2019_with_pred, y_predict_m2_df)
+brasileirao_2020_with_pred <- cbind(brasileirao_2020_with_pred, y_predict_m2_df)
 
-score_brasileirao_2019_m2_pred <- 
-  get_team_points_per_game(data = brasileirao_2019_with_pred, 
+score_brasileirao_2020_m2_pred <- 
+  get_team_points_per_game(data = brasileirao_2020_with_pred, 
                            y1 = quo(y1_pred_m2), 
                            y2 = quo(y2_pred_m2))
 
-points_per_game_obs_m2 <- merge(score_brasileirao_2019, 
-                             score_brasileirao_2019_m2_pred, 
-                             by = c("game_id", "team_id", "team_name"), 
-                             suffixes = c("_obs", "_est_m2")) 
+points_per_game_obs_m2 <- merge(score_brasileirao_2020, 
+                                score_brasileirao_2020_m2_pred, 
+                                by = c("game_id", "team_id", "team_name"), 
+                                suffixes = c("_obs", "_est_m2")) 
 
 points_per_game_obs_m2 |> 
   pivot_longer(cols = c(points_scored_obs, points_scored_est_m2)) |> 
@@ -152,19 +142,19 @@ pontuacao_m2 <- points_per_game_obs_m2 |>
   summarize(score_obs = sum(points_scored_obs), 
             score_est_m2 = sum(points_scored_est_m2))
 
-saveRDS(pontuacao_m2, "artifacts/pontuacao_total_m2.rds")
+# (pontuacao_m2, "artifacts/pontuacao_total_m2.rds")
 
 # Desempate rebaixamento
-pontuacao_m2 |> slice_min(score_est_m2, n=4)
-brasileirao_2019_with_pred |> 
-  filter(h == 11 | a == 11) |> 
-  summarize(sum(y1_pred_m2, y2_pred_m2))
+# pontuacao_m2 |> slice_min(score_est_m2, n=4)
+# brasileirao_2020_with_pred |> 
+#   filter(h == 11 | a == 11) |> 
+#   summarize(sum(y1_pred_m2, y2_pred_m2))
 
 ## MSE ----
-mse_m2 <- data.frame(mse_y1 = mse(brasileirao_2019_with_pred$y1, 
-                                  brasileirao_2019_with_pred$y1_pred_m2),
-                     mse_y2 = mse(brasileirao_2019_with_pred$y2, 
-                                     brasileirao_2019_with_pred$y2_pred_m2))
+mse_m2 <- data.frame(mse_y1 = mse(brasileirao_2020_with_pred$y1, 
+                                  brasileirao_2020_with_pred$y1_pred_m2),
+                     mse_y2 = mse(brasileirao_2020_with_pred$y2, 
+                                  brasileirao_2020_with_pred$y2_pred_m2))
 
 ## LOO ----
 
@@ -178,12 +168,12 @@ loo_y2_m2 <-loo(log_lik_y2_m2, r_eff = ref_y2_m2)
 
 # Model 3 ----
 
-brasileirao_2019_list_m3 <- list(G = nrow(brasileirao_2019), 
+brasileirao_2020_list_m3 <- list(G = nrow(brasileirao_2020), 
                                  T = 20,
-                                 h = brasileirao_2019$h,
-                                 a = brasileirao_2019$a,
-                                 y1 = brasileirao_2019$y1,
-                                 y2 = brasileirao_2019$y2,
+                                 h = brasileirao_2020$h,
+                                 a = brasileirao_2020$a,
+                                 y1 = brasileirao_2020$y1,
+                                 y2 = brasileirao_2020$y2,
                                  gamma1 = 1,
                                  gamma2 = 0)
 
@@ -191,7 +181,7 @@ m3 <- stan_model(file = "models/modelo2-poisson-bivariate.stan",
                  model_name = "model3-bivariate-gamma10")
 
 m3_fit <- sampling(object = m3, 
-                   data = brasileirao_2019_list_m3, 
+                   data = brasileirao_2020_list_m3, 
                    chains = 1, 
                    iter = 5000)
 
@@ -199,15 +189,15 @@ y_predict_m3 <- extract(m3_fit, pars = c("y1_tilde", "y2_tilde"))
 y_predict_m3_df <- data.frame(y1_pred_m3 = y_predict_m3$y1_tilde[1,], 
                               y2_pred_m3 = y_predict_m3$y2_tilde[1,])
 
-brasileirao_2019_with_pred <- cbind(brasileirao_2019_with_pred, y_predict_m3_df)
+brasileirao_2020_with_pred <- cbind(brasileirao_2020_with_pred, y_predict_m3_df)
 
-score_brasileirao_2019_m3_pred <- 
-  get_team_points_per_game(data = brasileirao_2019_with_pred, 
+score_brasileirao_2020_m3_pred <- 
+  get_team_points_per_game(data = brasileirao_2020_with_pred, 
                            y1 = quo(y1_pred_m3), 
                            y2 = quo(y2_pred_m3))
 
-points_per_game_obs_m3 <- merge(score_brasileirao_2019, 
-                                score_brasileirao_2019_m3_pred, 
+points_per_game_obs_m3 <- merge(score_brasileirao_2020, 
+                                score_brasileirao_2020_m3_pred, 
                                 by = c("game_id", "team_id", "team_name"), 
                                 suffixes = c("_obs", "_est_m3")) 
 
@@ -220,14 +210,14 @@ pontuacao_m3 <- points_per_game_obs_m3 |>
   summarize(score_obs = sum(points_scored_obs), 
             score_est_m3 = sum(points_scored_est_m3))
 
-saveRDS(pontuacao_m3, "artifacts/pontuacao_total_m3.rds")
+# saveRDS(pontuacao_m3, "artifacts/pontuacao_total_m3.rds")
 
 ## MSE ----
 
-mse_m3 <- data.frame(mse_y1 = mse(brasileirao_2019_with_pred$y1, 
-                                  brasileirao_2019_with_pred$y1_pred_m3),
-                     mse_y2 = mse(brasileirao_2019_with_pred$y2, 
-                                     brasileirao_2019_with_pred$y2_pred_m3))
+mse_m3 <- data.frame(mse_y1 = mse(brasileirao_2020_with_pred$y1, 
+                                  brasileirao_2020_with_pred$y1_pred_m3),
+                     mse_y2 = mse(brasileirao_2020_with_pred$y2, 
+                                  brasileirao_2020_with_pred$y2_pred_m3))
 
 ## LOO ----
 
@@ -241,12 +231,12 @@ loo_y2_m3 <-loo(log_lik_y2_m3, r_eff = ref_y2_m3)
 
 # Model 4 ----
 
-brasileirao_2019_list_m4 <- list(G = nrow(brasileirao_2019), 
+brasileirao_2020_list_m4 <- list(G = nrow(brasileirao_2020), 
                                  T = 20,
-                                 h = brasileirao_2019$h,
-                                 a = brasileirao_2019$a,
-                                 y1 = brasileirao_2019$y1,
-                                 y2 = brasileirao_2019$y2,
+                                 h = brasileirao_2020$h,
+                                 a = brasileirao_2020$a,
+                                 y1 = brasileirao_2020$y1,
+                                 y2 = brasileirao_2020$y2,
                                  gamma1 = 1,
                                  gamma2 = 1)
 
@@ -254,7 +244,7 @@ m4 <- stan_model(file = "models/modelo2-poisson-bivariate.stan",
                  model_name = "model3-bivariate-gamma11")
 
 m4_fit <- sampling(object = m4, 
-                   data = brasileirao_2019_list_m4, 
+                   data = brasileirao_2020_list_m4, 
                    chains = 1, 
                    iter = 5000)
 
@@ -262,15 +252,15 @@ y_predict_m4 <- extract(m4_fit, pars = c("y1_tilde", "y2_tilde"))
 y_predict_m4_df <- data.frame(y1_pred_m4 = y_predict_m4$y1_tilde[1,], 
                               y2_pred_m4 = y_predict_m4$y2_tilde[1,])
 
-brasileirao_2019_with_pred <- cbind(brasileirao_2019_with_pred, y_predict_m4_df)
+brasileirao_2020_with_pred <- cbind(brasileirao_2020_with_pred, y_predict_m4_df)
 
-score_brasileirao_2019_m4_pred <- 
-  get_team_points_per_game(data = brasileirao_2019_with_pred, 
+score_brasileirao_2020_m4_pred <- 
+  get_team_points_per_game(data = brasileirao_2020_with_pred, 
                            y1 = quo(y1_pred_m4), 
                            y2 = quo(y2_pred_m4))
 
-points_per_game_obs_m4 <- merge(score_brasileirao_2019, 
-                                score_brasileirao_2019_m4_pred, 
+points_per_game_obs_m4 <- merge(score_brasileirao_2020, 
+                                score_brasileirao_2020_m4_pred, 
                                 by = c("game_id", "team_id", "team_name"), 
                                 suffixes = c("_obs", "_est_m4")) 
 
@@ -283,14 +273,14 @@ pontuacao_m4 <- points_per_game_obs_m4 |>
   summarize(score_obs = sum(points_scored_obs), 
             score_est_m4 = sum(points_scored_est_m4))
 
-saveRDS(pontuacao_m4, "artifacts/pontuacao_total_m4.rds")
+# saveRDS(pontuacao_m4, "artifacts/pontuacao_total_m4.rds")
 
 ## MSE ----
 
-mse_m4 <- data.frame(mse_y1 = mse(brasileirao_2019_with_pred$y1, 
-                                  brasileirao_2019_with_pred$y1_pred_m4),
-                     mse_y2 = mse(brasileirao_2019_with_pred$y2, 
-                                     brasileirao_2019_with_pred$y2_pred_m4))
+mse_m4 <- data.frame(mse_y1 = mse(brasileirao_2020_with_pred$y1, 
+                                  brasileirao_2020_with_pred$y1_pred_m4),
+                     mse_y2 = mse(brasileirao_2020_with_pred$y2, 
+                                  brasileirao_2020_with_pred$y2_pred_m4))
 
 ## LOO ----
 
